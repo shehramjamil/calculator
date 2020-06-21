8531af40fc58684bd5d6b018420dd333d90988d3
Shehram <shahramjamil007@gmail.com>
2020-06-21 14:35:38 -0700

    Fibonacci class added.

---
+++ androidApp/src/main/kotlin/MainActivity.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package sample.calculator.android
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import sample.calculator.arithmeticparser.Fibonacci
+import sample.calculator.arithmeticparser.parseAndCompute
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val resultView = findViewById<TextView>(R.id.computed_result)
+        val input = findViewById<EditText>(R.id.input)
+
+        parseExpression.setOnClickListener {
+            val inputText = input.text.toString()
+            val result = parseAndCompute(inputText).expression
+            if (result != null)
+                resultView.text = result.toString()
+            else
+                resultView.text = "Unable to parse $inputText"
+        }
+
+        getFibonacci.setOnClickListener {
+            val number = input.text.toString()
+            val fibonacciOutput = Fibonacci(number).findFibonacciSum()
+            if (fibonacciOutput < 0)
+                resultView.text = getString(R.string.only_number)
+            else
+                resultView.text = fibonacciOutput.toString()
+
+        }
+    }
+}
+
